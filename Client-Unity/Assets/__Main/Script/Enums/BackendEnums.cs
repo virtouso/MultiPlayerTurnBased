ee3e830362f1e87f5d08df6cbc9bce234c26296a
LAPTOP-9KQD6BO4\s_moe <s.moeen.kamali@gmail.com>
2022-03-07 17:11:42 +0330

    boombastic

---
+++ Client-Unity/Assets/__Main/Script/Enums/BackendEnums.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TurnBasedMultiPlayer
+{
+    namespace Enum
+    {
+        namespace Backend
+        {
+            public enum BackendType
+            {
+                LocalHost,
+                Test,
+                Production
+            }
+        }
+    }
+}
---
+++ Client-Unity/Assets/__Main/Script/ScriptableObject/Backend/BackendRoutes.cs
@@ -0,0 +1,32 @@
+
+using UnityEngine;
+
+
+namespace Configuration
+{
+    namespace Backend
+    {
+        namespace General
+        {
+
+
+            public class BackendRoutes : ScriptableObject
+            {
+
+                
+                [System.Serializable]
+                public struct BackendUrls
+                {
+
+                    
+                }
+
+            }
+            
+            
+            
+            
+            
+        }
+    }
+}
